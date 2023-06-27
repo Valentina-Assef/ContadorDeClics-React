fea3b47ae3be023d119764118ed3705d220fe3fa
Valentina-Assef <valenassef@gmail.com>
2023-06-27 15:14:39 -0300

    functionality

--- contador-de-clics/src/App.js
+++ contador-de-clics/src/App.js
@@ -1,8 +1,41 @@
 import './App.css';
+import Boton from './componentes/Boton';
+import Contador from './componentes/Contador';
+import logo from './imagenes/logo.png';
+import { useState } from 'react';
 
 function App() {
+
+  const [numClics, setNumClics] = useState(0);
+
+  const manejarClic = () => {
+    setNumClics(numClics + 1);
+  };
+
+  const reiniciarContador = () => {
+    setNumClics(0);
+  };
+
   return (
-    <div className="App">
+    <div className='App'>
+      <div className='contenedor-logo'>
+        <img 
+          className='logo'
+          src={logo} 
+          alt='Logo'/>
+      </div>
+      <div className='contenedor-contador'>
+        <Contador 
+          numClics={numClics} />
+        <Boton
+          texto='Clic'
+          esBotonDeClic={true}
+          manejarClic={manejarClic} />
+        <Boton 
+          texto='Reiniciar'
+          esBotonDeClic={false}
+          manejarClic={reiniciarContador} />
+      </div>
     </div>
   );
 }
